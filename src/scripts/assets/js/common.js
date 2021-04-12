a5df64ac3106fa0dc9ca8be482f9bf6b64098a37
mono-shirakawa <shirakawa@monosus.co.jp>
2021-04-12 14:10:05 +0900

    new

--- src/scripts/assets/js/common.js
+++ src/scripts/assets/js/common.js
@@ -3,4 +3,12 @@
  *
 */
 
+import { add } from "lodash";
 import "../../modules/*";
+
+
+let menu = document.getElementById('#open');
+
+menu.addEventListener('click',() => {
+    alert('text');
+});
