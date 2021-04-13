708ee7ce59609f8c6f11d82ed308cf1278965121
mono-shirakawa <shirakawa@monosus.co.jp>
2021-04-13 13:18:53 +0900

    修正しました。

--- src/scripts/assets/js/daiki.js
+++ src/scripts/assets/js/daiki.js
@@ -2,9 +2,12 @@
 function toggleNav() {
     let body = document.body
     let menu = document.getElementById('l-header__hamburger');
-    let line = document.getElementsByClassName('l-hamburger__line');
+    let block = document.getElementById('l-header__block');
+
     menu.addEventListener('click', () => {
     menu.classList.toggle('is-open');
+    block.classList.toggle('nav-open');
+
     });
  }
  toggleNav();
